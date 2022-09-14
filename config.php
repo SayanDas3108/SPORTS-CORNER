43203bbaa30cacc89210721ca0be8b0e194492a0
Sayan Das <sayandas3108@gmail.com>
2022-09-14 22:41:43 +0530

    FirstCommit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "xxxxx"; //Secret Key
+$user = "xxxxxx"; //Secret Key
+$dbpass = "xxxxx"; //Secret Key
+$dbname = "xxxxxx"; //Secret Key
+
+
+$conn = mysqli_connect($host, $user, $dbpass, $dbname);
